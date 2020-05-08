86bc043543487fc0b8ca78c59bf1ec9ee4fcaf98
Dominic Lee <dominictwlee@gmail.com>
2020-05-08 17:37:03 +0100

    feat: ref instead of clone count el

--- src/bin/0_2_random_distribution.rs
+++ src/bin/0_2_random_distribution.rs
@@ -33,19 +33,19 @@ fn view(app: &App, Model { counts }: &Model, frame: Frame) {
   let win = app.window_rect();
   let win_width = win.len();
   let rect_width = win_width / (TOTAL as f32);
+  let rect_mid_point = rect_width / 2.0;
 
   draw.background().color(WHITE);
 
-  for (i, count) in counts.iter().enumerate() {
+  for (i, &count) in counts.iter().enumerate() {
     let max_left = win.left();
     let max_bottom = win.bottom();
     let offset_x = (i as f32) * rect_width;
-    let height = count.clone() as f32;
 
     draw
       .rect()
-      .x_y(max_left + (rect_width / 2.0) + offset_x, max_bottom)
-      .w_h(rect_width, height)
+      .x_y(max_left + rect_mid_point + offset_x, max_bottom)
+      .w_h(rect_width, count as f32)
       .color(GREY)
       .stroke_weight(2.0)
       .stroke(BLACK);
