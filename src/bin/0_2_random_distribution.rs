6a27f27a2ff749384698a67b383cb1a73657b7ce
Dominic Lee <dominictwlee@gmail.com>
2020-05-08 17:30:45 +0100

    feat: random dist chart

---
+++ src/bin/0_2_random_distribution.rs
@@ -0,0 +1,55 @@
+extern crate rand;
+use nannou::prelude::*;
+
+fn main() {
+  nannou::app(model).update(update).run();
+}
+
+struct Model {
+  counts: Vec<i32>,
+}
+
+const TOTAL: i32 = 20;
+
+fn model(app: &App) -> Model {
+  app.new_window().size(640, 360).view(view).build().unwrap();
+
+  let mut counts = Vec::new();
+
+  for _ in 0..TOTAL {
+    counts.push(0);
+  }
+
+  Model { counts }
+}
+
+fn update(_app: &App, Model { counts }: &mut Model, _update: Update) {
+  let index = random_range(0, TOTAL);
+  counts[index as usize] += 1;
+}
+
+fn view(app: &App, Model { counts }: &Model, frame: Frame) {
+  let draw = app.draw();
+  let win = app.window_rect();
+  let win_width = win.len();
+  let rect_width = win_width / (TOTAL as f32);
+
+  draw.background().color(WHITE);
+
+  for (i, count) in counts.iter().enumerate() {
+    let max_left = win.left();
+    let max_bottom = win.bottom();
+    let offset_x = (i as f32) * rect_width;
+    let height = count.clone() as f32;
+
+    draw
+      .rect()
+      .x_y(max_left + (rect_width / 2.0) + offset_x, max_bottom)
+      .w_h(rect_width, height)
+      .color(GREY)
+      .stroke_weight(2.0)
+      .stroke(BLACK);
+  }
+
+  draw.to_frame(app, &frame).unwrap();
+}
