5fdc6751b6326b1093340c9cf78c418c5763ef7c
Dominic Lee <dominictwlee@gmail.com>
2020-05-09 15:54:26 +0100

    feat: walker tend right

---
+++ src/bin/0_3_walker_right.rs
@@ -0,0 +1,63 @@
+extern crate rand;
+use nannou::prelude::*;
+
+fn main() {
+  nannou::app(model).update(update).run();
+}
+
+struct Model {
+  walker: Walker,
+}
+
+struct Walker {
+  x: i32,
+  y: i32,
+}
+
+impl Walker {
+  fn new() -> Walker {
+    Walker { x: 0, y: 0 }
+  }
+
+  fn step(&mut self) {
+    let r = random_f32();
+
+    if r < 0.4 {
+      self.x += 1;
+    } else if r < 0.6 {
+      self.x -= 1;
+    } else if r < 0.8 {
+      self.y += 1;
+    } else {
+      self.y -= 1;
+    }
+  }
+}
+
+fn model(app: &App) -> Model {
+  app.new_window().size(640, 360).view(view).build().unwrap();
+
+  Model {
+    walker: Walker::new(),
+  }
+}
+
+fn update(_app: &App, Model { walker }: &mut Model, _update: Update) {
+  walker.step();
+}
+
+fn view(app: &App, Model { walker }: &Model, frame: Frame) {
+  let draw = app.draw();
+
+  if app.elapsed_frames() == 1 {
+    draw.background().color(WHITE);
+  }
+
+  draw
+    .ellipse()
+    .x_y(walker.x as f32, walker.y as f32)
+    .w_h(2.0, 2.0)
+    .rgba(0.0, 0.0, 0.0, 1.0);
+
+  draw.to_frame(app, &frame).unwrap();
+}
